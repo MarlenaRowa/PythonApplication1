23e5c1b4f6a18fac4fc4a261b8f3698d5a73b0dd
MarlenaRowa <marlena.pirchl@rowa-automation.at>
2022-06-24 13:42:36 +0200

    3Subs

--- server.py
+++ server.py
@@ -10,9 +10,11 @@ import json
 app = Flask(__name__)
 dt = datetime.now()
 jsona = "Dziadek"
+jsonb = "Dziadek"
+jsonc = "Dziadek"
 
-@app.route("/notification/versionOnRobot", methods=['GET', 'POST'])
-def hello_world():
+@app.route("/notification/VersionReq", methods=['GET', 'POST'])
+def Sub1():
     print(request.path)
     print(request.method)
     print(request.json)
@@ -23,13 +25,42 @@ def hello_world():
 
     return ""
 
+@app.route("/notification/RobotToServer", methods=['GET', 'POST'])
+def Sub2():
+    print(request.path)
+    print(request.method)
+    print(request.json)
+    print(json.dumps(request.json, indent=4))
+
+    global jsonb
+    jsona = json.dumps(request.json)
+
+    return ""
+
+@app.route("/notification/ServerToRobot", methods=['GET', 'POST'])
+def Sub3():
+    print(request.path)
+    print(request.method)
+    print(request.json)
+    print(json.dumps(request.json, indent=4))
+
+    global jsonc
+    jsona = json.dumps(request.json)
+
+    return ""
+
 @app.route("/test")
 def wannacry():
     return jsona
     
 @app.route("/test2")
 def wannacry2():
-    return "Babcia"
+    return jsonb
+    
+        
+@app.route("/test2")
+def wannacry3():
+    return jsonc
     
 
 print (dt)
